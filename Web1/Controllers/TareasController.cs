e662f950f551ba1e49100b5ec42686d5711b262f
Frandy255 <frandymichelr25@gmail.com>
2025-08-01 17:04:34 -0400

    Segundo commit: Problemas para ejecutar el Sp

--- Web1/Controllers/TareasController.cs
+++ Web1/Controllers/TareasController.cs
@@ -5,13 +5,13 @@ using Web1.Models;
 using System.Security.Claims;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+using Microsoft.Data.SqlClient;
 
 namespace Web1.Controllers
 {
     public class TareasController : Controller
     {
         private readonly ApplicationDbContext context;
-        TareaModel tm = new TareaModel();
         public TareasController(ApplicationDbContext contxt) 
         {
             context = contxt;
@@ -31,22 +31,72 @@ namespace Web1.Controllers
         [HttpPost]
         public async Task<IActionResult> CrearTarea(TareaModel tma)
         {
-            if (ModelState.IsValid)
-            {
-             
-                tma.UserId = "E71C60E5-D581-4A2C-8764-A890F023FCBA";
-                context.Add(tma);
 
-                Console.WriteLine("IF");
-                await context.SaveChangesAsync();
-                return RedirectToAction(nameof(VerTareas));
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string nombre = tma.Name;
+                    string descrip = tma.Description;
+                    string estado = tma.Status;
+                    string prior = tma.Priority;
+                    DateTime inicio = DateTime.Now /*Convert.ToDateTime(tma.DateStart.ToString("yyyy-MM-dd HH:mm:ss"))*/;
+                    DateTime fin = DateTime.Now /*Convert.ToDateTime(tma.DateEnd.ToString("yyyy-MM-dd HH:mm:ss"))*/;
+                    string usuarioId = "E71C60E5-D581-4A2C-8764-A890F023FCBA";
+
+                    var parametros = new[]
+                    {
+                        new SqlParameter("@nombre", nombre),
+                        new SqlParameter("@descripcion", descrip),
+                        new SqlParameter("@estado", estado),
+                        new SqlParameter("@prioridad", prior),
+                        new SqlParameter("@inicio", inicio),
+                        new SqlParameter("@fin", fin),
+                        new SqlParameter("@idUsuario", usuarioId),
+                        new SqlParameter
+                        {
+                            ParameterName = "@result",
+                            SqlDbType = System.Data.SqlDbType.Int,
+                            Direction = System.Data.ParameterDirection.Output
+                        }
+
+
+                    };
+
+                    //Console.WriteLine(inicio.ToString());
+                    //Console.WriteLine(fin.ToString());
+
+                    await context.Database.ExecuteSqlRawAsync($"Exec pa_AgregarTareas @nombre, @descripcion, @estado,  @prioridad, @inicio, @fin, @idUsuario, @result output", parametros);
+
+                    int res = Convert.ToInt32(parametros[7].Value);
+
+                    Console.WriteLine(res.ToString());
+                  
+
+
+                    //tma.UserId = "E71C60E5-D581-4A2C-8764-A890F023FCBA";
+                    //context.Add(tma);
+
+                    //Console.WriteLine("IF");
+
+                  //  await context.SaveChangesAsync();
+                    return RedirectToAction(nameof(VerTareas));
+
+                }
+                else
+                {
+                    Console.WriteLine("else");
+
+                    return View("CrearTareas", tma);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                Console.WriteLine("else");
-
+                Console.WriteLine(ex.ToString());
                 return View("CrearTareas", tma);
+
             }
+
         }
 
         
